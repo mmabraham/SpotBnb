5852b53513f0154cc9439c54d93375da9f369796
Menachem Abraham <menachemabraham@gmail.com>
2017-06-20 18:39:27 -0400

    Create spots controller

--- app/controllers/api/spots_controller.rb
+++ app/controllers/api/spots_controller.rb
@@ -1,2 +1,27 @@
 class Api::SpotsController < ApplicationController
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def spot_params
+
+  end
 end
