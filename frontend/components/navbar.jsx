ce928a82d834da3b9e4ecd560d1f5fc7280b2534
Menachem Abraham <menachemabraham@gmail.com>
2017-06-20 09:32:27 -0400

    Update sample state

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -6,22 +6,48 @@ import { logout } from '../actions/session_actions';
 const Navbar = (props) => {
   if (props.currentUser) {
     return (
-      <header>
-        <h1>{`Welcome ${props.currentUser.username}!`}</h1>
-        <button onClick={props.logout}>
-          Logout
-        </button>
-      </header>
+      <nav className="main-nav nav-logged-in">
+        <ul className="right-nav-links">
+          <li>
+            <NavLink to="#">Host</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Trips</NavLink>
+          </li>
+          <li>
+            <button onClick={props.logout}>
+              Logout
+            </button>
+          </li>
+          <li>
+            <img src="" />
+          </li>
+        </ul>
+      </nav>
     );
   } else {
     return (
-      <header>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </header>
+      <nav className="main-nav nav-logged-out">
+        <ul className="right-nav-links">
+          <li>
+            <NavLink to="#">Host</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Trips</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Log In</NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </li>
+        </ul>
+      </nav>
     );
   }
 };
+
+// export default Navbar;
 
 const mapStateToProps = (state, ownProps) => {
   return {
