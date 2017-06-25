d008a2677ad2e0899db124ed91a2427715d8aa1a
Menachem Abraham <menachemabraham@gmail.com>
2017-06-25 16:20:03 -0400

    Refactor spot list item into seperate component

--- frontend/components/spots/spot_index.jsx
+++ frontend/components/spots/spot_index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Rating from 'react-rating';
-import { Link } from 'react-router-dom';
+import SpotIndexItem from './spot_index_item';
 
 export default class SpotIndex extends React.Component {
   constructor(props) {
@@ -9,26 +8,8 @@ export default class SpotIndex extends React.Component {
 
   render() {
     const spots = this.props.spots.map(spot => (
-        <div className="grid-list-item" key={spot.id}>
-          <Link to={`/spots/${spot.id}`}><img src={spot.img} /></Link>
-          <div className="list-item-info">
-            <div>{`$${spot.price}     ${spot.title}`}</div>
-            <div className="spot-list-subtitle">{`${spot.spot_type} · ${spot.capacity} beds`}</div>
-            <div>
-              <Rating
-                className="rating-stars"
-                placeholderRate={parseFloat(spot.rating)}
-                placeholder="fa fa-star fa-2x"
-                empty="fa fa-star-o fa-2x"
-                full="fa fa-star fa-2x"
-                readonly={true}
-              />
-            <span className='spot-list-rating'>{` ${spot.num_reviews} reviews` }</span>
-            </div>
-          </div>
-        </div>
-      )
-    );
+      <SpotIndexItem key={spot.id} spot={spot} />
+    ));
     return (
       <div className="grid-list-container ">
         <div className="grid-list" >
--- frontend/components/spots/spot_index_item.jsx
+++ frontend/components/spots/spot_index_item.jsx
@@ -1,1 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
+
+const SpotIndexItem = ({spot}) => (
+  <div className="grid-list-item" key={spot.id}>
+    <Link to={`/spots/${spot.id}`}><img src={spot.img} /></Link>
+    <div className="list-item-info">
+      <div>{`$${spot.price}     ${spot.title}`}</div>
+      <div className="spot-list-subtitle">{`${spot.spot_type} · ${spot.capacity} beds`}</div>
+      <div>
+        <Rating
+          className="rating-stars"
+          placeholderRate={parseFloat(spot.rating)}
+          placeholder="fa fa-star fa-2x"
+          empty="fa fa-star-o fa-2x"
+          full="fa fa-star fa-2x"
+          readonly={true}
+          />
+        <span className='spot-list-rating'>{` ${spot.num_reviews} reviews` }</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default SpotIndexItem;
