10f8ec0ea24a16adc6e726cc2fe5149ab620a7bd
Menachem Abraham <menachemabraham@gmail.com>
2017-06-25 17:12:41 -0400

    Add trip duration in tripInfo selector

--- frontend/components/trips/trip_index_item.jsx
+++ frontend/components/trips/trip_index_item.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom';
 import SpotIndexItem from '../spots/spot_index_item';
 
 const TripIndexItem = ({trip}) => {
+  console.log(trip.duration)
   return (
-    <li>
-      <SpotIndexItem spot={trip.spot} />
+    <li className="trip-item">
+      <SpotIndexItem spot={trip.spot}/>
+      <div className="trip-info">
+        <ul>
+          <li>Check in: {trip.start_date.toDateString()}</li>
+          <li>Check out: {trip.end_date.toDateString()}</li>
+          <li>Total: {`$${trip.duration * trip.spot.price}`}</li>
+          <li>Host: {trip.host.username}
+            <img src={trip.host.avatar}></img>
+          </li>
+        </ul>
+      </div>
     </li>
   )
 }
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -3,11 +3,15 @@ export const asArray = obj => Object.keys(obj).map((key) => obj[key]);
 export const tripsInfo = ({bookings, users, spots}) => {
   const trips = [];
   for (let key in bookings) {
+    const start_date = new Date(bookings[key].start_date);
+    const end_date = new Date(bookings[key].end_date);
     trips.push({
+      id: key,
       spot: spots[bookings[key].spot_id],
       host: users[spots[bookings[key].spot_id].host_id],
-      start_date: bookings[key].start_date,
-      end_date: bookings[key].end_date,
+      start_date,
+      end_date,
+      duration: Math.round((end_date.getTime() - start_date.getTime()) / (1000 * 60 * 60 * 24)),
     });
   }
   return trips;
