367c346be1e7ac71f812ffdbcd64f46b4a5d167c
Menachem Abraham <menachemabraham@gmail.com>
2017-06-27 19:30:38 -0400

    Update locationPicker to return place

--- frontend/components/search/location_picker.jsx
+++ frontend/components/search/location_picker.jsx
@@ -25,10 +25,10 @@ export default class LocationPicker extends React.Component {
   setupAutocomplete() {
     this.autocomplete = new google.maps.places.Autocomplete(this.place);
     google.maps.event.addListener(this.autocomplete, 'place_changed', () => {
-      const location = this.autocomplete.getPlace().geometry.location;
-      const lat = location.lat();
-      const lng = location.lng();
-      this.handler({lat, lng, location: this.place.value});
+      const place = this.autocomplete.getPlace()
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+      this.handler({lat, lng, location: this.place.value}, place);
     });
   }
 
--- frontend/components/search/searchbar_container.js
+++ frontend/components/search/searchbar_container.js
@@ -6,15 +6,16 @@ import { updateFilter } from '../../actions/filter_actions';
 const mapStateToProps = state => {
   return {
     filters: state.filters,
-  }
-}
+    changeMap: state.map.callback,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     updateFilter: (type, filter) => dispatch(updateFilter(type, filter)),
-  }
-}
+  };
+};
 
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(Searchbar)
-)
+);
