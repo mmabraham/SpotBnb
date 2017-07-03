67dba6d947b01d9886257cda1cabe587ec5d3c4d
Menachem Abraham <menachemabraham@gmail.com>
2017-07-03 15:03:33 -0400

    add webpack config for production env

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
-const path = require('path');
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/entry.jsx',
+  entry: "./frontend/entry.jsx",
   output: {
-    path: path.resolve(__dirname),
-    filename: './app/assets/javascripts/bundle.js'
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -19,8 +41,8 @@ module.exports = {
       }
     ]
   },
+  devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
-  devtool: 'source-map'
+    extensions: [".js", ".jsx", "*"]
+  }
 };
