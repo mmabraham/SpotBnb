8f2f9fb12a3410712e085362dd371193b3c6a8d3
Menachem Abraham <menachemabraham@gmail.com>
2017-07-21 14:45:05 -0400

    Add placeholder prop to LocationPicker component

--- frontend/components/search/location_picker.jsx
+++ frontend/components/search/location_picker.jsx
@@ -7,9 +7,10 @@ export default class LocationPicker extends React.Component {
     // optional props:
     // errors
     // location
+    // placeholder
 
     // required props:
-    // handler({lat, lng, location})
+    // handler({lat, lng, location}, DOMInputElement)
 
     this.handler = this.props.handler;
   }
@@ -31,14 +32,13 @@ export default class LocationPicker extends React.Component {
       this.handler({lat, lng, location: this.place.value}, place);
     });
   }
-
-  // <i className="fa fa-search" aria-hidden="true"></i>
+  
   render() {
     return (
       <label>
         <input
           ref={ref => this.place = ref}
-          placeholder={this.props.location || 'Address'}
+          placeholder={this.props.location || this.props.placeholder || 'Address'}
           className={this.props.errors && this.props.errors.lng ? 'invalid' : null}
         />
         <span className="errors" >{this.props.errors ? this.props.errors.lng : ''}</span>
