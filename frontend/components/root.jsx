356f3447a46346fcba99e87329014814bffd12e7
Menachem Abraham <menachemabraham@gmail.com>
2017-08-01 17:57:04 -0400

    Change material UI default color

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { teal600 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './app';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: teal600,
+  },
+});
+
 const Root = ({ store }) => (
   <Provider store={ store }>
     <HashRouter>
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={ muiTheme }>
         <App/>
       </MuiThemeProvider>
     </HashRouter>
